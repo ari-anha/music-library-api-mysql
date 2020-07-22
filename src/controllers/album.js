833ddfb5a8460224cd1f32c5de1f541fb8e8e49a
Arianha Bayley <arianhamb@gmail.com>
2020-07-22 13:22:58 +0100

    Written albumController.create

--- src/controllers/album.js
+++ src/controllers/album.js
@@ -1,1 +1,16 @@
+const { Album, Artist } = require('../models');
 
+exports.create = (req, res) => {
+  const { artistId } = req.params;
+  Artist.findByPk(artistId).then((artist) => {
+    if (!artist) {
+      res.status(404).json({ error: 'The artist could not be found.' });
+    } else {
+      Album.create(req.body).then((album) => {
+        album.setArtist(artistId).then((linkedAlbum) => {
+          res.status(201).json(linkedAlbum);
+        });
+      });
+    }
+  });
+};
