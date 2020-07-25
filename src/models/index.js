789574299a6b4203fc8f3517e28c300e90fca3d4
Arianha Bayley <arianhamb@gmail.com>
2020-07-25 17:25:14 +0100

    Changed models/index.js to try to deploy to heroku

--- src/models/index.js
+++ src/models/index.js
@@ -3,24 +3,8 @@ const ArtistModel = require('./artist');
 const AlbumModel = require('./album');
 const SongModel = require('./song');
 
-const {
-  DB_NAME,
-  DB_USER,
-  DB_PASSWORD,
-  DB_HOST,
-  DB_PORT,
-  CLEARDB_DATABASE_URL,
-} = process.env;
+const { DB_NAME, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT } = process.env;
 
-const sequelize = CLEARDB_DATABASE_URL
-  ? new Sequelize(CLEARDB_DATABASE_URL)
-  : new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
-      host: DB_HOST,
-      port: DB_PORT,
-      dialect: 'mysql',
-      logging: false,
-    });
-/*
 const setupDatabase = () => {
   const connection = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
     host: DB_HOST,
@@ -28,20 +12,21 @@ const setupDatabase = () => {
     dialect: 'mysql',
     logging: false,
   });
-*/
-const Artist = ArtistModel(connection, Sequelize);
-const Album = AlbumModel(connection, Sequelize);
-const Song = SongModel(connection, Sequelize);
 
-Album.belongsTo(Artist, { as: 'artist' });
-Song.belongsTo(Artist, { as: 'artist' });
-Song.belongsTo(Album, { as: 'album' });
+  const Artist = ArtistModel(connection, Sequelize);
+  const Album = AlbumModel(connection, Sequelize);
+  const Song = SongModel(connection, Sequelize);
 
-connection.sync({ alter: true });
-return {
-  Artist,
-  Album,
-  Song,
+  Album.belongsTo(Artist, { as: 'artist' });
+  Song.belongsTo(Artist, { as: 'artist' });
+  Song.belongsTo(Album, { as: 'album' });
+
+  connection.sync({ alter: true });
+  return {
+    Artist,
+    Album,
+    Song,
+  };
 };
 
-module.exports = sequelize;
+module.exports = setupDatabase;
