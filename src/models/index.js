54f6501d5fe43b02ff61c3ff6ff9ae887c2334de
Arianha Bayley <arianhamb@gmail.com>
2020-07-25 16:52:39 +0100

    Changed debug script from nodemon to node

--- src/models/index.js
+++ src/models/index.js
@@ -3,8 +3,24 @@ const ArtistModel = require('./artist');
 const AlbumModel = require('./album');
 const SongModel = require('./song');
 
-const { DB_NAME, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT } = process.env;
+const {
+  DB_NAME,
+  DB_USER,
+  DB_PASSWORD,
+  DB_HOST,
+  DB_PORT,
+  CLEARDB_DATABASE_URL,
+} = process.env;
 
+const sequelize = CLEARDB_DATABASE_URL
+  ? new Sequelize(CLEARDB_DATABASE_URL)
+  : new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
+      host: DB_HOST,
+      port: DB_PORT,
+      dialect: 'mysql',
+      logging: false,
+    });
+/*
 const setupDatabase = () => {
   const connection = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
     host: DB_HOST,
@@ -12,21 +28,20 @@ const setupDatabase = () => {
     dialect: 'mysql',
     logging: false,
   });
+*/
+const Artist = ArtistModel(connection, Sequelize);
+const Album = AlbumModel(connection, Sequelize);
+const Song = SongModel(connection, Sequelize);
 
-  const Artist = ArtistModel(connection, Sequelize);
-  const Album = AlbumModel(connection, Sequelize);
-  const Song = SongModel(connection, Sequelize);
+Album.belongsTo(Artist, { as: 'artist' });
+Song.belongsTo(Artist, { as: 'artist' });
+Song.belongsTo(Album, { as: 'album' });
 
-  Album.belongsTo(Artist, { as: 'artist' });
-  Song.belongsTo(Artist, { as: 'artist' });
-  Song.belongsTo(Album, { as: 'album' });
-
-  connection.sync({ alter: true });
-  return {
-    Artist,
-    Album,
-    Song,
-  };
+connection.sync({ alter: true });
+return {
+  Artist,
+  Album,
+  Song,
 };
 
-module.exports = setupDatabase();
+module.exports = sequelize;
