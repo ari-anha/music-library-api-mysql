c0c2a3497189066606ee3638d41bd377ed18d377
Arianha Bayley <arianhamb@gmail.com>
2020-07-27 17:35:40 +0100

    Changed models/index.js, changed all 'connection' keyword to 'sequelize', and added setupDatabase again

--- src/models/index.js
+++ src/models/index.js
@@ -12,28 +12,30 @@ const {
   CLEARDB_DATABASE_URL,
 } = process.env;
 
-const sequelize = CLEARDB_DATABASE_URL
-  ? new Sequelize(CLEARDB_DATABASE_URL)
-  : new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
-      host: DB_HOST,
-      port: DB_PORT,
-      dialect: 'mysql',
-      logging: false,
-    });
+const setupDatabase = () => {
+  const sequelize = CLEARDB_DATABASE_URL
+    ? new Sequelize(CLEARDB_DATABASE_URL)
+    : new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
+        host: DB_HOST,
+        port: DB_PORT,
+        dialect: 'mysql',
+        logging: false,
+      });
 
-const Artist = ArtistModel(sequelize, Sequelize);
-const Album = AlbumModel(sequelize, Sequelize);
-const Song = SongModel(sequelize, Sequelize);
+  const Artist = ArtistModel(sequelize, Sequelize);
+  const Album = AlbumModel(sequelize, Sequelize);
+  const Song = SongModel(sequelize, Sequelize);
 
-Album.belongsTo(Artist, { as: 'artist' });
-Song.belongsTo(Artist, { as: 'artist' });
-Song.belongsTo(Album, { as: 'album' });
+  Album.belongsTo(Artist, { as: 'artist' });
+  Song.belongsTo(Artist, { as: 'artist' });
+  Song.belongsTo(Album, { as: 'album' });
 
-connection.sync({ alter: true });
-return {
-  Artist,
-  Album,
-  Song,
+  sequelize.sync({ alter: true });
+  return {
+    Artist,
+    Album,
+    Song,
+  };
 };
 
 module.exports = setupDatabase;
